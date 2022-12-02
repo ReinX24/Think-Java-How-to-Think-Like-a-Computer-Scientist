ad53b99b21bc38349ae25dc9f686060adc31220d
Rein <reinX244@gmail.com>
2022-12-02 19:55:33 +0800

    Added TempConverter features
    Added code that will catch exceptions such as the user inputting strings
    instead of integers.
    
    When an exception happens, it will return to the start of the program.
    
    Added else statement for number not being part of the choices, this will
    return the user to the main menu as well.
    
    Main menu is put inside a function instead of being part of the main
    method.
    
    When the user inputs a string instead of a number in the part where a
    temperature value is asked, it will return them to the menu.

--- Think Java: How to Think Like a Computer Scientist/3 - Input and Output/TempConverter.java
+++ Think Java: How to Think Like a Computer Scientist/3 - Input and Output/TempConverter.java
@@ -1,62 +1,103 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 class TempConverter {
 
+    // Make a scanner input that will accept the values
+    // Scanner is outside the main method for other methods or functions will use it
+    static Scanner scanOne = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        // Make a scanner input that will accept the values
+        boolean converterRun = true;
 
-        Scanner scanOne = new Scanner(System.in);
+        while (converterRun == true) {
 
-        // Instantiate the values
+            try {
 
-        // Choice to convert celcius to fahrenheit or vice versa
+                // Choice to convert celcius to fahrenheit or vice versa
+
+                int userChoice = menuChoice();
+
+                if (userChoice == 1) {
+
+                    // Instantiate the variables as you ask user input
+                    System.out.println("[Celcius to Fahrenheit Converter]");
+                    System.out.print("Enter value you wish to convert: ");
+                    float celciusTemp = scanOne.nextFloat();
+
+                    // Convert celcius to fahrenheit
+
+                    float fahrenheitTemp = celciusTemp * 9 / 5 + 32;
+
+                    // // round off to nearest tenths
+
+                    // fahrenheitTemp = Math.round(fahrenheitTemp * 100) / 100;
+
+                    // Show the converted values
+
+                    System.out.printf("Celcius: %.0f C\n", celciusTemp); // rounds off to a whole number
+                    System.out.printf("Fahrenheit: %.1f F\n", fahrenheitTemp); // rounds off nearest tenths
+
+                }
+
+                else if (userChoice == 2) {
+
+                    System.out.println("[Fahrenheit to Celcius Converter]");
+                    System.out.print("Enter value you wish to convert: ");
+                    float fahrenheitTemp = scanOne.nextFloat();
+
+                    // Convert fahrenheit to celcius
+
+                    float celciusTemp = (fahrenheitTemp - 32) * 5 / 9;
+
+                    // Show the converted values
+
+                    System.out.printf("Fahrenheit: %.1f F\n", fahrenheitTemp); // tenths
+                    System.out.printf("Celcius: %.0f C\n", celciusTemp); // whole number
+
+                }
+
+                else if (userChoice == 3) {
+
+                    System.out.println("[Converter turned off]");
+                    scanOne.close(); // closes scanner
+                    converterRun = false; // breaks the while loop
+
+                }
+
+                else {
+
+                    System.out.println("[Number not a choice, try again]");
+                    scanOne.nextLine();
+
+                }
+
+            }
+
+            catch (InputMismatchException ex) {
+
+                System.out.println("[Invalid input, try again]");
+                scanOne.nextLine();
+
+            }
+
+            // scanOne.close(); // closes the scanner once the conversion is done
+            // converterRun = false; // breaks the loop that keeps the program running
+
+        }
+
+    }
+
+    public static int menuChoice() {
 
         System.out.println("[1] Celcius to Fahrenheit");
         System.out.println("[2] Fahrenheit to Celcius");
+        System.out.println("[3] Close Converter");
         System.out.print("User Input: ");
         int userChoice = scanOne.nextInt();
 
-        if (userChoice == 1) {
-
-            System.out.println("[Celcius to Fahrenheit Converter]");
-            System.out.print("Enter value you wish to convert: ");
-            float celciusTemp = scanOne.nextFloat();
-
-            // Convert celcius to fahrenheit
-
-            float fahrenheitTemp = celciusTemp * 9 / 5 + 32;
-
-            // // round off to nearest tenths
-            
-            // fahrenheitTemp = Math.round(fahrenheitTemp * 100) / 100;
-
-            // Show the converted values
-
-            System.out.printf("Celcius: %.0f C\n", celciusTemp); // rounds off to a whole number
-            System.out.printf("Fahrenheit: %.1f F\n", fahrenheitTemp); // rounds off nearest tenths
-
-        }
-
-        else if (userChoice == 2) {
-
-            System.out.println("[Fahrenheit to Celcius Converter]");
-            System.out.print("Enter value you wish to convert: ");
-            float fahrenheitTemp = scanOne.nextFloat();
-
-            // Convert fahrenheit to celcius
-
-            float celciusTemp = (fahrenheitTemp - 32) * 5/9;
-
-            // Show the converted values
-
-            System.out.printf("Fahrenheit: %.1f F\n", fahrenheitTemp); // tenths
-            System.out.printf("Celcius: %.0f C\n", celciusTemp); // whole number
-
-        }
-
-        // Closing scanner
-        scanOne.close();
+        return userChoice;
 
     }
 
