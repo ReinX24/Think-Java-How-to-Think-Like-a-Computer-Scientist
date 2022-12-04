9257600c649f6b6151b59b1ee75fd43c15db2407
Rein <reinX244@gmail.com>
2022-12-04 18:48:27 +0800

    Array Positions Exercise Added/ To be Finished An exercise where we convert arrays into different arrays, have not yet finished this exercise.

---
+++ Think Java: How to Think Like a Computer Scientist/Personal Exercises/ArrayPositionExercise.java
@@ -0,0 +1,27 @@
+public class ArrayPositionExercise {
+
+    public static void main(String[] args) {
+        
+        /*
+         * Do an exercise that will turn the following:
+         * 
+         * [1, 2, 3]
+         * [4, 5, 6]
+         * [7, 8, 9]
+         * 
+         * Into:
+         * 
+         * [1, 4, 7]
+         * [2, 5, 8]
+         * [3, 6, 9]
+         * 
+         */
+
+         // 2D array that will contain all of our numbers
+         int[][] numArray = {[1, 2, 3],[4, 5, 6],[7, 8, 9]}
+
+         // For each loops that will iterate through the 2D array
+
+    }
+    
+}
