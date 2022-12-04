d67318fc43339845ebcaa987db7b169af74f2bfd
Rein <reinX244@gmail.com>
2022-12-04 19:25:24 +0800

    Finished Array Positions Exercise Finished an exercise where we convert a 2D array and changed its positions.
    Exercise Presentation:
    Old Arrays:
    [1, 2, 3]
    [4, 5, 6]
    [7, 8, 9]
    
    New Arrays:
    [1, 4, 7]
    [2, 5, 8]
    [3, 6, 9]

--- Think Java: How to Think Like a Computer Scientist/Personal Exercises/ArrayPositionExercise.java
+++ Think Java: How to Think Like a Computer Scientist/Personal Exercises/ArrayPositionExercise.java
@@ -1,7 +1,10 @@
+import javax.sound.sampled.SourceDataLine;
+import javax.xml.crypto.dsig.spec.SignatureMethodParameterSpec;
+
 public class ArrayPositionExercise {
 
     public static void main(String[] args) {
-        
+
         /*
          * Do an exercise that will turn the following:
          * 
@@ -17,11 +20,65 @@ public class ArrayPositionExercise {
          * 
          */
 
-         // 2D array that will contain all of our numbers
-         int[][] numArray = {[1, 2, 3],[4, 5, 6],[7, 8, 9]}
+        // 2D array that will contain all of our numbers
+        int[][] numArray = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
-         // For each loops that will iterate through the 2D array
+        // 2D array that will contain the new set of numbers
+        int[][] newNumArray = new int[3][3];
 
+        // For each loops that will iterate through the numArray
+
+        for (int i = 0; i < numArray.length; i++) { // for outer array
+
+            for (int j = 0; j < numArray[i].length; j++) { // for inner array
+
+                if (numArray[i][j] == numArray[i][j]) {
+
+                    newNumArray[j][i] = numArray[i][j];
+
+                    // Logic Explanation for new array
+                    // j = 0, 1, 2 (outer arrays iterated through)
+                    // i = 0 (first element of each of these arrays)
+                    // loop break
+                    // j = 0, 1, 2
+                    // i = 1
+                    // loop break
+                    // j = 0, 1, 2
+                    // i = 2
+                    // loop break
+                    // looping ends
+
+                }
+
+            }
+
+        }
+
+        // Printing the old an new array
+        System.out.println("Old Array: ");
+        for (int i = 0; i < numArray.length; i++) { // for outer array
+
+            for (int j = 0; j < numArray[i].length; j++) { // for inner array
+
+                System.out.print(" | " + numArray[i][j] + " | "); // prints elements of each array
+
+            }
+
+            System.out.println(); // makes a new line after printing the inner array elements
+
+        }
+
+        System.out.println("New Array: ");
+        for (int i = 0; i < newNumArray.length; i++) { // for outer array
+
+            for (int j = 0; j < newNumArray[i].length; j++) { // for inner array
+
+                System.out.print(" | " + newNumArray[i][j] + " | "); // prints elements of each array
+
+            }
+
+            System.out.println(); // makes a new line after printing the inner array elements
+
+        }
     }
-    
 }
