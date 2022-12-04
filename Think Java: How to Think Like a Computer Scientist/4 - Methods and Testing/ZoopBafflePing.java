eeec9ca66511e8c63920d3e71746d9e1c78d1819
Rein <reinX244@gmail.com>
2022-12-04 19:53:07 +0800

    Finished Zoop Baffle Ping Finished exercise 4.2 in Think Java Book

---
+++ Think Java: How to Think Like a Computer Scientist/4 - Methods and Testing/ZoopBafflePing.java
@@ -0,0 +1,63 @@
+public class ZoopBafflePing {
+
+    public static void main(String[] args) {
+
+        System.out.print("No, I ");
+        zoop();
+        System.out.print("I ");
+        baffle();
+
+        // Program Flow
+        // No, I
+        // Go to zoo method
+        // baffle method
+        // No, I wug
+        // ping method
+        // No, I wug. (println so new line)
+        // go back to zoop
+        // No, I wug.
+        // You wugga
+        // baffle method
+        // No, I wug.
+        // You wugga wug
+        // ping method
+        // No, I wug.
+        // You wugga wug.
+        
+        // Prints I
+        // I
+        // baffle method
+        // I wug
+        // ping method
+        // I wug.
+
+        // Final output: 
+        
+        // No, I wug.
+        // You wugga wug.
+        // I wug.
+
+    }
+
+    public static void zoop() {
+
+        baffle();
+        System.out.print("You wugga ");
+        baffle();
+
+    }
+
+    public static void baffle() {
+
+        System.out.print("wug");
+        ping();
+
+    }
+
+    public static void ping() {
+
+        System.out.println(".");
+
+    }
+
+}
