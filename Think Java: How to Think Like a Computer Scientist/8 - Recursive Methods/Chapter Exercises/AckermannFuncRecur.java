b5f7616a0bbb5bff0695f201a13e289e7eedcc7f
Rein <reinX244@gmail.com>
2023-01-05 13:26:25 +0800

    Added Ackermann Function (Exercise 8.4) Solution

--- Think Java: How to Think Like a Computer Scientist/8 - Recursive Methods/Chapter Exercises/AckermannFuncRecur.java
+++ Think Java: How to Think Like a Computer Scientist/8 - Recursive Methods/Chapter Exercises/AckermannFuncRecur.java
@@ -10,6 +10,21 @@ public class AckermannFuncRecur {
          * 
          */
 
+        System.out.println(AckFunc(0, 0)); // returns 1
+        System.out.println(AckFunc(1, 0)); // returns 1
+        System.out.println(AckFunc(1, 1)); // returns 2
+        System.out.println(AckFunc(2, 2)); // returns 3
+
+    }
+
+    public static int AckFunc(int m, int n) {
+        if (m == 0) { // first condition
+            return n + 1;
+        } else if (m > 0 && n == 0) { // second condition
+            return AckFunc(m - 1, n);
+        } else { // no condition checker needed since this is the last one
+            return AckFunc(m - 1, AckFunc(m, n - 1));
+        }
     }
 
 }
