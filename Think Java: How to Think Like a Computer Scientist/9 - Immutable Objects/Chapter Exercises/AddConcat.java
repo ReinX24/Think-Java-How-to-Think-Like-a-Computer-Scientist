f4e7e66f5ee7c7ee7c2a4850a08d2d0b366a9f07
Rein <reinX244@gmail.com>
2023-01-13 10:52:17 +0800

    AddConcat Exercise Answers added (partial)

--- Think Java: How to Think Like a Computer Scientist/9 - Immutable Objects/Chapter Exercises/AddConcat.java
+++ Think Java: How to Think Like a Computer Scientist/9 - Immutable Objects/Chapter Exercises/AddConcat.java
@@ -11,10 +11,26 @@ public class AddConcat {
         // Row 1
         System.out.println(true + " pog"); // only boolean works for 1st row
         // Row 2
-        System.out.println('a' + 'a'); // char works, results in a number
+        System.out.println('a' + 'a'); // char works, results in a number (ASCII decimal value)
         System.out.println('a' + 5); // int works, results in a number
         System.out.println('a' + 1.555); // double works, results in a number
         System.out.println("Pog " + 'a'); // concatenates normally
+        // Row 3
+        System.out.println(1 + 'a'); // adds to int equivalent of the char
+        System.out.println(1 + 3); // int & int
+        System.out.println(1 + 1.256); // int & double
+        System.out.println(2 + " number"); // int & String
+        // Row 4
+        System.out.println(5.56 + 'a'); // double & char
+        System.out.println(5.25 + 2); // double & int
+        System.out.println(2.45 + 1.65); // double & double
+        System.out.println(5.556 + " double"); // double & String
+        // Row 5
+        System.out.println("Boolean " + false); // String & boolean
+        System.out.println("Char " + 'a'); // String & char
+        System.out.println("int " + 5); // String & int
+        System.out.println("double " + 6.34563); // String & double
+        System.out.println("Hello " + "World"); // String & String
 
     }
 
