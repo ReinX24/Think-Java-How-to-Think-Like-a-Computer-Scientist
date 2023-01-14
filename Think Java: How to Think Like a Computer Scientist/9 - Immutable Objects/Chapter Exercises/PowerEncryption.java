398bc8741bfaf6d474b89363c0525f4a94d784c1
Rein <reinX244@gmail.com>
2023-01-14 18:20:47 +0800

    Changed PowerEncryption - PowNumRecur function

--- Think Java: How to Think Like a Computer Scientist/9 - Immutable Objects/Chapter Exercises/PowerEncryption.java
+++ Think Java: How to Think Like a Computer Scientist/9 - Immutable Objects/Chapter Exercises/PowerEncryption.java
@@ -11,21 +11,26 @@ public class PowerEncryption {
          * 
          */
 
-        System.out.println(powNum(3, 2));
+        System.out.println(powNumLoop(6, 2));
+        System.out.println(powNumRecur(3, 2));
         System.out.println(powNumFast(5, 2));
         System.out.println(powNumBigInt(17, 17));
 
     }
 
-    public static int powNum(int x, int n) {
-        if (n == 0) {
+    public static int powNumLoop (int x, int n) {
+        int powNumResult = 1;
+        for (int i = 0; i < n; i++) {
+            powNumResult *= x;
+        }
+        return powNumResult;
+    }
+
+    public static int powNumRecur(int x, int n) {
+        if (n != 0) {
+            return x * powNumRecur(x, n - 1);
+        }  else {
             return 1;
-        } 
-        int powNumResult = powNum(x, n - 1);
-        if (n > 0) {
-            return  x * powNumResult;
-        } else {
-            return powNumResult;
         }
 
     }
