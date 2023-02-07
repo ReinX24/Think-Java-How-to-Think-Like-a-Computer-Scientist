8a09d2c91f88420e2506da2fc89a4068c5ddc6d6
Rein <reinX244@gmail.com>
2023-02-07 15:50:47 +0800

    Chapter Examples in development

--- 14 - Objects of objects/Chapter Examples/src/CardCollection.java
+++ 14 - Objects of objects/Chapter Examples/src/CardCollection.java
@@ -10,7 +10,7 @@ public class CardCollection {
         this.labelStr = labelStr; // assigns a String for labelStr
         this.cardArrList = new ArrayList<Card>(); // completes the instantiation of ArrayList
     }
-    
+
     /* Method that adds a Card to the ArrayList */
     public void addCard(Card paraCard) {
         cardArrList.add(paraCard);
@@ -21,10 +21,49 @@ public class CardCollection {
         return cardArrList.remove(cardIndex);
     }
 
-    /* Method that removes a Card from the end of the ArrayList and returns that Card object */
+    /*
+     * Method that removes a Card from the end of the ArrayList and returns that
+     * Card object
+     */
     public Card popCard() { // no parameters this time
         int i = cardArrList.size() - 1; // goes to the end of the ArrayList
         return cardArrList.remove(i); // returns the removed Card
     }
+
+    /* Method that returns the size of the ArrayList in a CardCollection object */
+    public int size() {
+        return cardArrList.size(); // returns the size of the array list
+    }
+
+    /* Method that checks if the ArrayList is empty or not */
+    public boolean empty() {
+        return cardArrList.size() == 0; // returns either true or false
+    }
+
+    /*
+     * Removes cards from a CardCollection and adds to the CardCollection in the
+     * parameter
+     */
+    public void deal(CardCollection cardCol, int n) {
+        /* Removes n card from a CardCollection and adds same amount to cardCol */
+        for (int i = 0; i < n; i++) {
+            Card removedCard = popCard(); // removes a card from the object it is invoked on
+            cardCol.addCard(removedCard); // adds removed card to cardCol
+        }
+    }
+
+    /* Getter for an element in the ArrayList within CardCollection */
+    public Card getCard(int i) {
+        return cardArrList.get(i); // gets Card at index i
+    }
+
+    /* Getter for the last card in ArrayList, does not remove it */
+    public Card lastCard() {
+        Card lastCard = cardArrList.get(size() - 1);
+        return lastCard;
+    }
+
+    /* Method that swaps the places of two cards */
+    // TODO: swapCards method
 
 }
--- 14 - Objects of objects/Chapter Examples/src/CardDeck.java
+++ 14 - Objects of objects/Chapter Examples/src/CardDeck.java
@@ -2,16 +2,16 @@ public class CardDeck {
 
     public static void main(String[] args) {
         
-        Deck deckOne = new Deck();
-        deckOne.shuffleDeck(); // shuffles deck
+        // Deck deckOne = new Deck();
+        // deckOne.shuffleDeck(); // shuffles deck
         // deckOne.selectionSort(); // sorts deck
         // Deck subDeckOne = deckOne.subDeck(0, 4); // first 5 cards in deckOne
         // Deck subDeckTwo = deckOne.subDeck(8, 12);
         // Deck deckTwo = Deck.mergeSort(subDeckOne, subDeckTwo);
         // System.out.println(deckTwo.toString());
         // deckOne = deckOne.mergeSortRecursive();
-        deckOne.insertionSort();
-        System.out.println(deckOne.toString());
+        // deckOne.insertionSort();
+        // System.out.println(deckOne.toString());
 
     }
     
